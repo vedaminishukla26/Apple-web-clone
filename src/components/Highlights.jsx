e7bcdd602449d47a5512078e33b4aa2f01b235aa
vedaminishukla26 <vedaminishukla26@gmail.com>
2025-03-14 16:51:57 +0530

    Create Top, Highlight section

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,8 +1,44 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 import React from 'react'
+import { rightImg, watchImg } from '../utils'
 
 function Highlights() {
+
+  useGSAP(() => {
+    gsap.to('#title', {
+      opacity: 1,
+      y: 0
+    })
+    gsap.to('.link', {
+      opacity: 1, 
+      y: 0,
+      stagger: 0.25
+    })
+  }, [])
+
   return (
-    <div>Highlights</div>
+    <section id="highlights" className='w-screen overflow-hidden h-full common-padding bg-zinc'>
+      <div className='max-screen-width' >
+        <div className='mb-12 w-full md:flex items-end justify-between' >
+          <h1 id='title' className='section-heading' >
+            Get the highlights
+          </h1>
+          <div className='flex flex gap-5 items-end'>
+            <p className='link' >
+              Watch the film
+              <img src={watchImg} alt='watch' className='ml-2'>
+              </img>
+            </p>
+            <p className='link' >
+              Watch the event
+              <img src={rightImg} alt='right' className='ml-2'>
+              </img>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
